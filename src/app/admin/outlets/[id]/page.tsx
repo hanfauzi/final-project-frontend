2d99bb19cc90b0a32af58dccac416d437843b793
YUS / PIS <yusrilhafis@gmail.com>
2025-10-02 08:08:56 +0700

    fix: props in outlet detail page

--- src/app/admin/outlets/[id]/page.tsx
+++ src/app/admin/outlets/[id]/page.tsx
@@ -1,16 +1,9 @@
 "use client";
 
-import { FC } from "react";
-import OutletDetail from "../../_components/OutletDetail";
 import { useParams } from "next/navigation";
+import OutletDetail from "../../_components/OutletDetail";
 
-interface OutletPageProps {
-  params: {
-    id: string;
-  };
-}
-
-const OutletPage: FC<OutletPageProps> = () => {
+const OutletPage = () => {
     const params = useParams()
     const id = params?.id as string
 
